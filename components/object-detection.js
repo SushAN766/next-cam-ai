96f1ebb123fde0d6c3d22a33c2eefbe4f3b02464
SushAN766 <sushanthbs766@gmail.com>
2025-04-21 23:28:54 +0530

    Setup the AI Model

--- components/object-detection.js
+++ components/object-detection.js
@@ -1,10 +1,21 @@
 "use client"
-import React, {useEffect,useRef} from 'react'
+import React, {useEffect,useRef,useState} from 'react'
 import Webcam from "react-webcam";
-
+import * as tf from "@tensorflow/tfjs";
+import {load as cocoSSDLoad} from "@tensorflow-models/coco-ssd";
+let detectInterval;
 const ObjectDetection = () => {
-
+  const [isLoading, setIsLoading] =useState(true);
   const webcamRef =useRef(null);
+  const runCoco=async () => {
+    setIsLoading(true);
+    const net = await cocoSSDLoad();
+    setIsLoading(false);
+
+    detectInterval=setInterval(() => {
+      //runObjectDetection(net);
+    }, 10); 
+  };
   const showmyVideo = () => {
     if (
       webcamRef.current !== null &&
@@ -19,24 +30,27 @@ const ObjectDetection = () => {
   };
 
   useEffect(() => {
-    
+    runCoco();
     showmyVideo();
   }, []);
 
   return (
-    <div className="mt-8" >
-    <div className="relative flex justify-center items-center gradient p-1.5 rounded-md ">
-       
-        {/*webcam*/}
-        <Webcam
+    <div className="mt-8">
+      {isLoading ? (
+        <div className="gradient-text">Loading AI Model...</div>
+      ) : (
+        <div className="relative flex justify-center items-center gradient p-1.5 rounded-md">
+          {/* webcam */}
+          <Webcam
             ref={webcamRef}
             className="rounded-md w-full lg:h-[720px]"
             muted
           />
-        {/* canvas */}
-        
+          {/* canvas */}
+          
         </div>
-        </div>
+      )}
+    </div>
   );
 };
 
